272c5de3632bbeb49738a1eb0e83e562c04c5d60
aniketpati <aniketp209@gmail.com>
2023-11-01 23:22:18 +0530

    end of section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private accountService: AccountService) { }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
